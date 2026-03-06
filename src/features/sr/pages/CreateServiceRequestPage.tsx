809f69be5ac022630fc5e60d39b4409dcd116880
abhiezytek <abhijit.pharale@gmail.com>
2026-03-06 13:54:04 +0530

    Revert commit 3bd8fb72 by restoring CreateServiceRequestPage.tsx to its previous contents.

---
+++ src/features/sr/pages/CreateServiceRequestPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateServiceRequestPage = () => {
+    return (
+        <div>
+            <h1>Create Service Request Page</h1>
+        </div>
+    );
+};
+
+export default CreateServiceRequestPage;
